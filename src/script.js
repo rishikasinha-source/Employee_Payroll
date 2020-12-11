049037ba27c438186758308c68a18af3a70b4abe
rishika7-hub <srishika970@gmail.com>
2020-12-12 00:57:03 +0530

    Set Event Listner on salary range

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const salary=document.querySelector('#salary');
+        const output=document.querySelector('.salary-output');
+        salary.addEventListener('input',function(){
+            output.textContent=salary.value;
+        });
+
