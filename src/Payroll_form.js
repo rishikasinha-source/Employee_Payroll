5be4b194d188911be2bf9041d51e11301dfb0252
rishika7-hub <srishika970@gmail.com>
2020-12-13 02:13:25 +0530

    On document load set event listeners

--- src/Payroll_form.js
+++ src/Payroll_form.js
@@ -1,9 +1,27 @@
+window.addEventListener('DOMContentLoaded',(event) => {
+	const name=document.querySelector('#name');
+	const textError=document.querySelector('.text-error');
+	name.addEvenListener('input', function() {
+		if(name.value.length==0) {
+			textError.textContent="";
+			return;
+		}
+		try {
+			(new EmployeePayrollData()).name=name.value;;
+			textError.textContent="";
+		  } catch (e) {
+			textError.textContent=e;
+		 }
+	});
+
 const salary=document.querySelector('#salary');
 const output=document.querySelector('.salary-output');
 output.textContent=salary.value;
 salary.addEventListener('input',function(){
     output.textContent=salary.value;
 });
+
+
 document.getElementById("submit").onclick = function() {
     let employee = new EmployeePayroll();
     employee.name = document.getElementById("name").value;
